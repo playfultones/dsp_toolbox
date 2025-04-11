70ca8d0100f2389eaa5b127ec581b414752221d1
Bence Kovács <23636204+githappens@users.noreply.github.com>
2025-04-11 09:01:17 +0200

    Refactor applyLimiter function documentation and remove unused threshold parameter

--- dynamics/limiter.h
+++ dynamics/limiter.h
@@ -8,16 +8,15 @@ namespace PlayfulTones::DspToolBox
     static constexpr auto kSmallValue = 1e-6f;
     /*
         * Apply a simple limiter to the audio buffer.
-        * This function applies a hard limiter to the audio samples in the buffer.
-        * It reduces the amplitude of samples that exceed a specified threshold.
+        * This function applies a hard limiter to the audio samples in the buffer
+        * ensuring that no sample exceeds the specified ceiling value.
         *
         * @param buffer The audio buffer containing the samples to be limited.
         * @param numChannels The number of channels in the audio buffer.
         * @param numFrames The number of frames in the audio buffer.
-        * @param threshold The threshold above which samples will be limited. Default is 1.0f.
         * @param ceiling The maximum allowed amplitude after limiting. Default is 1.0f.
     */
-    inline void applyLimiter (float** buffer, int numChannels, int numFrames, float threshold = 1.f, float ceiling = 1.f)
+    inline void applyLimiter (float** buffer, int numChannels, int numFrames, float ceiling = 1.f)
     {
         for (int ch = 0; ch < numChannels; ch++)
         {
