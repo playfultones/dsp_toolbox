2021d61a480758b39ef3e19114441fae51b4bf1f
Bence Kovács <23636204+githappens@users.noreply.github.com>
2025-05-08 21:56:16 +0200

    Add constants header

---
+++ core/constants.h
@@ -0,0 +1,13 @@
+/*******************************************************************
+* Copyright         : 2025 Playful Tones
+* Author            : Bence Kovács
+* License           : GNU General Public License v3.0
+*******************************************************************/
+
+#pragma once
+
+namespace PlayfulTones::DspToolBox::Constants
+{
+    static constexpr float pi = static_cast<float> (3.141592653589793238L);
+    static constexpr float twoPi = static_cast<float> (2 * pi);
+} // namespace PlayfulTones::DspToolBox::Constants
