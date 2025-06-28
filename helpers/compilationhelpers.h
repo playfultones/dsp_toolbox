24701615239369ce43a23539719318aaebd3eec9
Bence Kovács <23636204+githappens@users.noreply.github.com>
2025-06-28 18:37:16 +0200

    Add markUsed function to suppress unused parameter warnings

---
+++ helpers/compilationhelpers.h
@@ -0,0 +1,24 @@
+/*******************************************************************
+* Copyright         : 2025 Playful Tones
+* Author            : Bence Kovács
+* License           : GNU General Public License v3.0
+*******************************************************************/
+
+#pragma once
+
+namespace PlayfulTones::DspToolBox
+{
+    /**
+     * @brief A no-op function to mark unused parameters.
+     *
+     * This function can be used to avoid compiler warnings about unused parameters.
+     * It does nothing and is intended for use in template code where some parameters
+     * may not be used in every instantiation.
+     *
+     * @param args The unused parameters.
+     */
+    template <typename... Args>
+    void markUsed (Args&&...)
+    {
+    }
+} // namespace PlayfulTones::DspToolBox
